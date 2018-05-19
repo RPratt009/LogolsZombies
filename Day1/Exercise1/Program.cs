b4705ed254837fe15f400adeb74f2ff608b62750
RPratt009 <17344024+RPratt009@users.noreply.github.com>
2018-05-19 15:12:38 -0400

    Updating my notes and adding  exercise1, its a little bit fancy

---
+++ Day1/Exercise1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Exercise1 {
+ class Program {
+  private static string GetStatus(int health) {
+   string alive = "Alive";
+   string zombie = "Zombie";
+   string dead = "Dead";
+   string unknown = "Unknown";
+   string text = " > Status: ";
+   /*
+   if (health == 1) {
+    return text + alive;
+   } else if (health == 2) {
+    return text + zombie;
+   } else if (health == 3) {
+    return text + dead;
+   } else {
+    return text + unknown;
+   }
+   /*/
+   //double sided to swap btwn if and switch faster
+      switch (health) {
+       case 1:
+        return text + alive;
+       case 2:
+        return text + zombie;
+       case 3:
+        return text + dead;
+       default:
+        return text + unknown;
+      }
+   
+  }
+  static string CapFirst(string s) {
+   char[] a = s.ToCharArray();
+   a[0] = char.ToUpper(a[0]);
+   return new string(a);
+  }
+  static void Main(string[] args) {
+   int i = 0;
+   string[] survivorList={"john","steve","tom","dick","harry","jacob","joe","billy","ashely","rebecca","johny","mike","nathan","jerry","william","michelle","nicole","allen","keith","anthony"};
+   int[] statusList={1,1,1,1,1,1,1,1,4,3,3,3,1,1,1,1,2,3,2,2};
+   Console.WriteLine("List of survivors ");
+   foreach(string name in survivorList) {
+    Console.WriteLine(i+1 + " " + CapFirst(name.ToString()) + GetStatus(statusList[i]));
+    i++;
+   }
+  }
+ }
+}
--- Day1/testproject/Program.cs
+++ Day1/testproject/Program.cs
@@ -4,8 +4,33 @@ namespace testproject
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+            //cli is command line interface
+            //dotnet new console
+            /*
+            going to do the rest of this in a block just because
+            mkdir (folername)
+            ; the semicolon ends each statement
+            {} or curly Parenthesis for the block of code
+            logic operators
+            == && != >= (and other variations)
+            */
+            string myString = "A true value";
+            int myInt = 9;
+            if (myInt == 9)
+            {
+                Console.WriteLine(myString);
+            }
+
+            int number = 0;
+            while (number < 3)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
             Console.WriteLine("Hello World!");
         }
     }
