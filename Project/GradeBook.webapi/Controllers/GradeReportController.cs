8ba28983d7d3f91e2b182296a0cf75fa85cf3a7f
RPratt009 <17344024+RPratt009@users.noreply.github.com>
2018-07-07 15:25:44 -0400

    addingin project

---
+++ Project/GradeBook.webapi/Controllers/GradeReportController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GradeBook.webapi.Controllers
+{
+    [Route("api/[controller]")]
+    public class GradeReport : Controller
+    {
+        GradeReportRepository repository;
+
+        public GradeReportController()
+        {
+            repository = new GradeReportRepository();
+        }
+
+        // GET api/GradeReport
+        [HttpGet]
+        public IEnumerable<GradeReport> Get()
+        {
+            return repository.GetAll();
+        }
+
+        // GET api/GradeReport/5
+        [HttpGet("{id}")]
+        public GradeReport Get(int id)
+        {
+            return repository.Get(id);
+        }
+
+        // POST api/GradeReport
+        [HttpPost]
+        public void Post([FromBody]GradeReport personStatus)
+        {
+            repository.Insert(personStatus);
+        }
+
+        // PUT api/GradeReport/5
+        [HttpPut]
+        public void Put([FromBody]GradeReport personStatus)
+        {
+            repository.Update(personStatus);
+        }
+
+        // DELETE api/GradeReport/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+    }
+}
+
---
+++ Project/GradeBook.webapi/GradeBookReport.cs
@@ -0,0 +1,17 @@
+namespace GradeBook.webapi
+{
+    using MySql.Data.MySqlClient;
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Text;
+
+    public class GradeBookReport
+    {
+        public int PersonID { get; set; }
+        public int GradeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LetterGrade { get; set; }
+    }
+}
