7663c3f145160e218eb1ca9b11910b2ef4effb4b
RPratt009 <17344024+RPratt009@users.noreply.github.com>
2018-07-21 11:16:39 -0400

    Had to make some changes to the sql db, but this is done and working

--- Review/ZombieReport.webapi/ZombieReport/src/app/zombieReport.component.ts
+++ Review/ZombieReport.webapi/ZombieReport/src/app/zombieReport.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { PersonStatus } from './entities/PersonStatus'
+import { PersonStatusService } from './services/PersonStatusService'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'zombieReport',
+  templateUrl: './zombieReport.component.html',
+  styleUrls: ['./zombieReport.component.css']
 })
 export class ZombieReportComponent {
-  title = 'app';
+  public title = 'zombieReport';
+  public statuses:PersonStatus[] = [];
+
+  public constructor(personStatusService: PersonStatusService) {
+    personStatusService.getAll().subscribe(result => {
+      for (let status of result) {
+        this.statuses.push(status);
+      }
+    });
+ 
+  }
 }
